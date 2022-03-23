a840ada3696328da93581f9c658f0ed473efb791
Mwau stephen <stephenmwau41@gmail.com>
2022-03-23 16:38:42 +0300

    implemented the success logic

--- script.js
+++ script.js
@@ -7,10 +7,11 @@ const displayGuess = document.querySelector(".guess");
 const displayMessage = document.querySelector(".message");
 const ScoreLabel = document.querySelector(".label-score");
 const labelScore = document.querySelector(".score");
-const highScoreLabel = document.querySelector(".label-highscore");
+const highScoreLabel = document.querySelector(".highscore");
+const bodyEl = document.querySelector("body");
 
-let guessNumber = Math.trunc(Math.random() * 20 + 1);
-console.log(guessNumber);
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+console.log(secretNumber);
 let score = 20;
 let highscore = 0;
 
@@ -23,8 +24,17 @@ btnCheck.addEventListener("click", function () {
   //   1.check for empty value
   if (!guess) {
     messageDisplay("No number!🤦‍♂️");
-  }
-  if (guessNumber === guess) {
+  } else if (secretNumber === guess) {
+    if (score > highscore) {
+      highscore = score;
+      highScoreLabel.textContent = highscore;
+    }
+    bodyEl.style.background = "#ff4500";
+    displayNumber.style.width = "30rem";
+    displayMessage.textContent = "Correct answer 🏆🏆";
+
     displayNumber.textContent = guess;
+  } else if (secretNumber !== guess) {
+    ScoreLabel.textContent = score--;
   }
 });
